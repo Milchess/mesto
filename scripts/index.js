012298fd7ce71bd8c450a6b2caff12f18699b710
Ksenia Milchenko <milch.ks.2992@gmail.com>
2022-07-26 23:18:00 +0500

    новый попап и кнопка лайк

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,53 @@
-let formElement = document.querySelector('.popup');
-let nameInput = formElement.querySelector('.popup__user_type_name');
-let jobInput = formElement.querySelector('.popup__user_type_vocation');
+let popupProfile = document.querySelector('#popup-profile');
+let nameInput = popupProfile.querySelector('.popup__user_type_name');
+let jobInput = popupProfile.querySelector('.popup__user_type_vocation');
 let editButton = document.querySelector('.profile__edit-button');
-let popupClose = formElement.querySelector('.popup__button-close');
+let popupCloseProfile = popupProfile.querySelector('#popup-close-profile');
 let userName = document.querySelector('.profile__user-name');
 let userVocation = document.querySelector('.profile__user-vocation');
+
+let popupCard = document.querySelector('#popup-card');
+let addButton = document.querySelector('.profile__add-button');
+let popupCloseCard = popupCard.querySelector('#popup-close-card');
+
+let likeActives = document.querySelectorAll('.grid-card__like');
+
+for (let likeActive of likeActives) {
+    likeActive.addEventListener('click', activeLike);
+}
+
+function activeLike() {
+    this.classList.add('grid-card__like_active');
+}
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     userName.textContent = nameInput.value;
     userVocation.textContent = jobInput.value;
-    closePopup();
+    closePopupProfile();
 }
 
-function openPopup() {
-    formElement.classList.add('popup_opened');
+function openPopupProfile() {
+    popupProfile.classList.add('popup_opened');
     nameInput.value = userName.textContent;
     jobInput.value = userVocation.textContent;
 }
 
-function closePopup() {
-    formElement.classList.remove('popup_opened');
+function closePopupProfile() {
+    popupProfile.classList.remove('popup_opened');
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-editButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+function openPopupCard() {
+    popupCard.classList.add('popup_opened');
+}
+
+function closePopupCard() {
+    popupCard.classList.remove('popup_opened');
+}
+
+popupProfile.addEventListener('submit', formSubmitHandler);
+popupCard.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openPopupProfile);
+addButton.addEventListener('click', openPopupCard);
+popupCloseProfile.addEventListener('click', closePopupProfile);
+popupCloseCard.addEventListener('click', closePopupCard);
